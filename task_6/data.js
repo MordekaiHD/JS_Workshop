349e96ac6ad99baa4ac6261a6e5b1f946d8e195d
Your Name <you@example.com>
2024-08-10 15:13:03 +0300

    task_6

---
+++ task_6/data.js
@@ -0,0 +1,17 @@
+const postsData = `[
+  {
+      "img": "https://http.cat/images/100.jpg",
+      "h3": "100",
+      "p": "100 Continue"
+  },
+  {
+      "img": "https://http.cat/images/102.jpg",
+      "h3": "102",
+      "p": "102 Processing"
+  },
+  {
+      "img": "https://http.cat/images/200.jpg",
+      "h3": "200",
+      "p": "200 OK"
+  }
+]`;
