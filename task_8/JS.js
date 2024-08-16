154bfddb69a5d0172e4df030a69ecf4bfecb0517
Your Name <you@example.com>
2024-08-16 16:03:59 +0300

    task_8

--- task_8/JS.js
+++ task_8/JS.js
@@ -1,21 +1,18 @@
 const data = JSON.parse(featuredItems);
 const products = document.querySelector('.products');
+const cartItems = document.querySelector('.cart__Items');
+
 
 data.forEach(element => {
   const content__products = document.createElement('div');
   content__products.classList.add('content__products');
   products.appendChild(content__products);
 
-  const linkProduct = document.createElement('a');
-  linkProduct.classList.add('products__link');
-  linkProduct.href = '#';
-  content__products.appendChild(linkProduct);
-
   const imgProduct = document.createElement('img');
   imgProduct.src = element.img;
   imgProduct.alt = "foto";
   imgProduct.classList.add('img__prod');
-  linkProduct.appendChild(imgProduct);
+  content__products.appendChild(imgProduct);
 
   const infoProducts = document.createElement('div');
   infoProducts.classList.add('info__products');
@@ -35,4 +32,67 @@ data.forEach(element => {
   priceProduct.classList.add('price');
   priceProduct.textContent = element.price;
   infoProducts.appendChild(priceProduct);
+
+  imgProduct.addEventListener('mouseover', () => {
+    imgProduct.style.transition = 'filter 0.5s';
+    imgProduct.style.filter = 'brightness(50%)';
+  });
+
+  imgProduct.addEventListener('mouseout', () => {
+    imgProduct.style.transition = 'filter 0.5s';
+    imgProduct.style.filter = 'brightness(100%)';
+
+  });
+
+  imgProduct.addEventListener('click', () => {
+    
+    const productsCart = document.createElement('div');
+    productsCart.classList.add('products__cart');
+    cartItems.appendChild(productsCart);
+    shoppingCartVisibility();
+
+    const imgCart = document.createElement('img');
+    imgCart.src = element.img;
+    imgCart.alt = "foto";
+    imgCart.classList.add('img__cart');
+    productsCart.appendChild(imgCart);
+
+    const infoCart = document.createElement('div');
+    infoCart.classList.add('info__cart');
+    productsCart.appendChild(infoCart);
+
+    const titleCart = document.createElement('h3');
+    titleCart.classList.add('title__cart');
+    titleCart.textContent = element.title;
+    infoCart.appendChild(titleCart);
+
+    const textCart = document.createElement('p');
+    textCart.classList.add('text__cart');
+    textCart.textContent = element.text;
+    infoCart.appendChild(textCart);
+
+    const priceCart = document.createElement('p');
+    priceCart.classList.add('price');
+    priceCart.textContent = element.price;
+    infoCart.appendChild(priceCart);
+
+    const buutonRemuve = document.createElement('button');
+    buutonRemuve.classList.add('buuton__remuve');
+    buutonRemuve.textContent = 'Remover do carrinho';
+    productsCart.appendChild(buutonRemuve);
+
+    buutonRemuve.addEventListener('click', () => {
+      productsCart.remove();
+      shoppingCartVisibility();
+    });
+  });
+
+  function shoppingCartVisibility() {
+    if (cartItems.children.length > 0) {
+      cartItems.style.display = 'block';
+    } else {
+      cartItems.style.display = 'none';
+    }
+  }
+
 });
--- task_8/data.js
+++ task_8/data.js
@@ -3,13 +3,13 @@ const featuredItems  = `[
     "img" : "img/products/products1.svg",
     "title" : "ELLERY X M'O CAPSULE",
     "text" : "Known for her sculptural takes ontraditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-    "price" : "$52.00"
+    "price" : "$42.00"
   },
   {
     "img" : "img/products/products2.svg",
     "title" : "ELLERY X M'O CAPSULE",
     "text" : "Known for her sculptural takes ontraditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-    "price" : "$52.00"
+    "price" : "$55.00"
   },
   {
     "img" : "img/products/products3.svg",
@@ -21,18 +21,18 @@ const featuredItems  = `[
     "img" : "img/products/products4.svg",
     "title" : "ELLERY X M'O CAPSULE",
     "text" : "Known for her sculptural takes ontraditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-    "price" : "$52.00"
+    "price" : "$33.00"
   },
   {
     "img" : "img/products/products5.svg",
     "title" : "ELLERY X M'O CAPSULE",
     "text" : "Known for her sculptural takes ontraditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-    "price" : "$52.00"
+    "price" : "$48.00"
   },
   {
     "img" : "img/products/products6.svg",
     "title" : "ELLERY X M'O CAPSULE",
     "text" : "Known for her sculptural takes ontraditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-    "price" : "$52.00"
+    "price" : "$56.00"
   }
 ]`;
