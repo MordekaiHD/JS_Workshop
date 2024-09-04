b1a8e24465f3f3b91ddbb9c33582117a65769590
Your Name <you@example.com>
2024-09-04 17:55:13 +0300

    Introduction to Node.js

--- Introduction to Node.js/JS.js
+++ Introduction to Node.js/JS.js
@@ -1,19 +1,18 @@
 const http = require('http');
 
-// Счетчики просмотров для каждой страницы
 let homeViewCount = 0;
 let aboutViewCount = 0;
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
-    homeViewCount++;  // Увеличиваем счетчик для главной страницы
+    homeViewCount++; 
     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
     res.write(`<h1>Главная страница</h1>`);
     res.write(`<p>Эта страница была просмотрена ${homeViewCount} раз(а).</p>`);
     res.write(`<a href="/about">Перейти на страницу "О нас"</a>`);
     res.end();
   } else if (req.url === '/about') {
-    aboutViewCount++;  // Увеличиваем счетчик для страницы "О нас"
+    aboutViewCount++; 
     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
     res.write(`<h1>Страница "О нас"</h1>`);
     res.write(`<p>Эта страница была просмотрена ${aboutViewCount} раз(а).</p>`);
