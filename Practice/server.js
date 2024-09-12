bc52543867341491278f4df469cd8da1142ad09f
Your Name <you@example.com>
2024-09-12 13:10:43 +0300

    s

--- Practice/server.js
+++ Practice/server.js
@@ -6,27 +6,24 @@ const app = express();
 const PORT = 3000;
 const COUNTER_FILE = path.join(__dirname, 'counter.json');
 
-// Функция для загрузки счетчика из файла
+
 function loadCounter() {
   if (!fs.existsSync(COUNTER_FILE)) {
-    return { '/': 0, '/about': 0 }; // начальные значения
+    return { '/': 0, '/about': 0 };
   }
   const data = fs.readFileSync(COUNTER_FILE, 'utf-8');
   return JSON.parse(data);
 }
 
-// Функция для сохранения счетчика в файл
 function saveCounter(counter) {
   fs.writeFileSync(COUNTER_FILE, JSON.stringify(counter, null, 2));
 }
 
-// Загрузим значения счетчиков при запуске сервера
 let counter = loadCounter();
 
-// Главная страница
 app.get('/', (req, res) => {
-  counter['/'] = (counter['/'] || 0) + 1; // Увеличиваем счетчик
-  saveCounter(counter); // Сохраняем в файл
+  counter['/'] = (counter['/'] || 0) + 1;
+  saveCounter(counter);
   res.send(`
         <h1>Корневая страница</h1>
         <p>Количество просмотров: ${counter['/']}</p>
@@ -34,10 +31,9 @@ app.get('/', (req, res) => {
     `);
 });
 
-// Страница "О нас"
 app.get('/about', (req, res) => {
-  counter['/about'] = (counter['/about'] || 0) + 1; // Увеличиваем счетчик
-  saveCounter(counter); // Сохраняем в файл
+  counter['/about'] = (counter['/about'] || 0) + 1;
+  saveCounter(counter);
   res.send(`
         <h1>Страница about</h1>
         <p>Количество просмотров: ${counter['/about']}</p>
@@ -45,7 +41,7 @@ app.get('/about', (req, res) => {
     `);
 });
 
-// Запуск сервера
+
 app.listen(PORT, () => {
   console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
